cd9e19faa34cbee875f4afd62d3424e876cb7431
Cintrian <velegoj2@yandex.ru>
2022-09-06 21:36:07 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
-        if (age >= 18) {
-            System.out.println("Поздравляю с совершеннолетием!");
-        } else {
-            System.out.println("Извини, придется немного подождать");
+        int childAge = 6;
+        if (childAge >= 7 && childAge < 18) {
+            System.out.println("Ты ходишь в школу");
+        } else if (childAge >=18 && childAge < 24) {
+            System.out.println("Ты закончил школу, можешь поступать в университет");
+        } else if (childAge >=24){
+            System.out.println("Ты окончил университет, пора искать первую работу");
         }
     }
 }
