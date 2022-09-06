d46bc78a19f4b0e938a7bf520e2d959480d6851d
Cintrian <velegoj2@yandex.ru>
2022-09-06 22:24:54 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 24;
-        boolean kindergartenAge = age >= 2 && age <= 6;
-        boolean schoolAge = age >= 7 && age <= 18;
-        boolean universityAge = age > 18 && age < 24;
-        boolean workAge = age >= 24;
-        if (kindergartenAge) {
-            System.out.println("Если возраст человека " + age + " то ему нужно ходить в детский сад");
-        } else if (schoolAge) {
-            System.out.println("Если возраст человека " + age + " то ему нужно ходить в школу");
-        } else if (universityAge) {
-            System.out.println("Если возраст человека " + age + " то ему нужно ходить в университет");
-        } else if (workAge) {
-            System.out.println("Если возраст человека " + age + " то ему пора ходить на работу");
+        int age = 14;
+        if (age<5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        } else if (age>=14) {
+            System.out.println("Ребенок может кататься без сопровождения");
+        } else {
+            System.out.println("Ребенок может кататься в сопровождении взрослого");
         }
     }
 }
