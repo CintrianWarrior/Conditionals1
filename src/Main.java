3e4711b2c2cf70dc95aa8c7d88eef1192a56834c
Cintrian <velegoj2@yandex.ru>
2022-09-06 22:32:41 +0300

    Task 9

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 14;
-        if (age<5) {
-            System.out.println("Ребенок не может кататься на аттракционе");
-        } else if (age>=14) {
-            System.out.println("Ребенок может кататься без сопровождения");
+        int one = 44;
+        int two = 20;
+        int three = 100;
+        if (one>two&&one>three) {
+            System.out.println("Первое число большее");
+        } else if (two>one&&two>three) {
+            System.out.println("Второе число большее");
         } else {
-            System.out.println("Ребенок может кататься в сопровождении взрослого");
+            System.out.println("Третье число большее");
         }
     }
 }
